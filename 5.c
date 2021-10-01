4e45de7892675a67117086844d55571deeb9e11e
Aaron3154518 <aaoren210@gmail.com>
2021-10-01 11:17:30 -0400

    Added initial solution for problem 5 Added .gitignore

---
+++ 5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char input[256];
+
+	printf("Type Words: ");
+	fflush(stdout);
+
+	gets(input);
+
+	printf("Printing every 3rd word:\n");
+
+	int ctr = 0;
+	char* loc = strtok(input, " ");
+	while (loc != NULL) {
+		if (++ctr % 3 == 0) {
+			printf("%s\n", loc);
+		}
+		loc = strtok(NULL, " ");
+	}
+
+	return 0;
+}
